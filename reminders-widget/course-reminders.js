341cd4b8446403c62e5d59a3aa4c3601e90a25ce
Jennifer Wagner <49212830+jenniferwagner18@users.noreply.github.com>
2025-05-21 10:50:19 -0700

    Update course-reminders.js

--- reminders-widget/course-reminders.js
+++ reminders-widget/course-reminders.js
@@ -1,17 +1,9 @@
 window.addEventListener('load', (loadEvent) => {
-
-// update filesPath to where you saved image files, either in Public Files or Manage Files in a course
-
-    const filesPath = '/shared/reminders-widget/';
-    const lockedIcon = filesPath + 'locked.svg';
-    const unlockedIcon = filesPath + 'unlocked.svg';
-    const checkIcon = filesPath + 'check.svg';
-    const exclamIcon = filesPath + 'exclamation.svg';
-    
+ 
 // check course status and start/end dates (Course Admin -> Course Offering Information)
     
-        const statusElem = document.getElementById('courseActiveStatus');
-        const statusImg = document.getElementById('courseStatusImg');
+        const statusElem = document.getElementById('statusCheck');
+        const statusImg = document.getElementById('statusImg');
         const statusStart = document.getElementById('startDate');
         const statusEnd = document.getElementById('endDate');
           
@@ -26,12 +18,10 @@ window.addEventListener('load', (loadEvent) => {
 
             if (courseData.IsActive) {
                 statusElem.innerText= "ACTIVE. This course is visible to students in their My Courses list. Students will have access to the course between the start and end dates or if no dates are set.";
-                statusImg.src = unlockedIcon;
-                statusImg.alt = statusImg.title = "Course is ACTIVE - students can access course within start and end dates";
+                statusImg.className = 'fa fa-unlock green';
             } else { 
                 statusElem.innerText = "NOT ACTIVE. Please make this course active so it is visible to students in their My Courses list, then students will have access to the course between the start and end dates or if no dates are set.";
-                statusImg.src = lockedIcon;
-                statusImg.alt = statusImg.title = "Course is NOT ACTIVE - students cannot access course!"; 
+                statusImg.className = 'fa fa-lock red';
             }
             
             const startDate = courseData.StartDate === null ? null : new Date(courseData.StartDate);
@@ -86,30 +76,24 @@ window.addEventListener('load', (loadEvent) => {
             throw new Error('Error fetching Content/root data');
         }
         
-        // Tip: to check if any content items exist, use .length > 0 instead of .some method: if (contentData.length > 0)
-
         // check for syllabus in module titles or topic titles
 
         if (contentData.some(item => item.Title.toLowerCase().includes("syllabus") || item.Structure.some(content => content.Title.toLowerCase().includes("syllabus")))){
             syllabusElem.innerText = "This course includes a Content module or item with the word syllabus in the title, which is clearly labeled for students. Please ensure that it is an accessible webpage or Word document.";
-            syllabusImg.src = checkIcon;
-            syllabusImg.alt = syllabusImg.title = "Syllabus found in Content";
+            syllabusImg.className = 'fa fa-check-circle green';
         } else { 
             syllabusElem.innerText= "This course does NOT include a Content module or item with the word syllabus in the title. Make sure to add your syllabus as an accessible webpage or Word document. Please do not upload it to the Overview area or any description areas.";
-            syllabusImg.src = exclamIcon;
-            syllabusImg.alt = syllabusImg.title = "Syllabus NOT found in Content";
+            syllabusImg.className = 'fa fa-exclamation-triangle orange';
         }    
 
         // check for long descriptions
 
         if (contentData.some(item => item.Description.Text.length > 500)) {
             descElem.innerText = "This course includes at least one long module description. Please ensure that all course materials are created as accessible webpages or Word documents in Content and are not added to the description areas.";
-            descImg.src = exclamIcon;
-    	    descImg.alt = descImg.title = "LONG descriptions on modules found in course";
+            descImg.className = 'fa fa-exclamation-triangle orange';
         } else { 
             descElem.innerText = "This course includes short or empty descriptions on modules in Content. Please continue to avoid adding course materials to description areas and only add them as accessible webpages and Word documents in Content.";
-    	    descImg.src = checkIcon;
-    	    descImg.alt = descImg.title = "Short or empty descriptions on modules in course";
+    	    descImg.className = 'fa fa-check-circle green';
         }
     } catch (error) {
         console.error('Error fetching Content data:', error);
@@ -133,12 +117,10 @@ window.addEventListener('load', (loadEvent) => {
 
             if (filesData.some(item => item.Name.toLowerCase().includes(".mp4") || item.Name.toLowerCase().includes(".mov"))){
                 videoElem.innerText= "At least one .mp4 or .mov video file was found in Manage Files in this course. Please upload videos to MediaSpace, proofread the captions, and embed them into D2L instead of uploading them directly to D2L courses.";
-                videoImg.src = exclamIcon;
-                videoImg.alt = videoImg.title = "Videos found in Manage Files";
+                videoImg.className = 'fa fa-exclamation-triangle orange';
             } else { 
                 videoElem.innerText = "No .mp4 or .mov video files were found in Manage Files in this course. If you plan to add videos, please upload them to MediaSpace, proofread the captions, and embed them into D2L instead of uploading them directly to your course.";
-                videoImg.src = checkIcon;
-                videoImg.alt = videoImg.title = "Videos NOT found in Manage Files"; 
+                videoImg.className = 'fa fa-check-circle green'; 
             }   
     
         } catch (error) {
@@ -164,12 +146,10 @@ window.addEventListener('load', (loadEvent) => {
         
             if (schemesData.some(scale => scale.Ranges.some(range => range.Symbol === "4.0" || range.Symbol === "3.5" || range.Symbol === "3.0"))){
                 gpaElem.innerText= "At least one grade scheme with GPAs was found in this course. Please ensure to apply this scheme to the Final Calculated (or Adjusted) Grade so that you can submit final grades directly from D2L.";
-                gpaImg.src = checkIcon;
-                gpaImg.alt = gpaImg.title = "GPA grade scheme found in course";
+                gpaImg.className = 'fa fa-check-circle green';
             } else { 
                 gpaElem.innerText = "No grade schemes with GPAs were found in this course. Please ensure to create and apply a 4.0 GPA scheme to the Final Calculated (or Adjusted) Grade so that you can submit final grades directly from D2L.";
-                gpaImg.src = exclamIcon;
-                gpaImg.alt = gpaImg.title = "GPA grade scheme NOT found in course";
+                gpaImg.className = 'fa fa-exclamation-triangle orange';
             }   
         } catch (error) {
             console.error('Error fetching Schemes data:', error);
